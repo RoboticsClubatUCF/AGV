de0187ed40fb7a48c9b933705efb530d2d889f70
Marc <mas1660@knights.ucf.edu>
2021-12-17 22:13:50 -0500

    We have the locations now, but the code is a bit broken... I'll need to clean it up a bit.

--- ugv_vision/script/pothole.py
+++ ugv_vision/script/pothole.py
@@ -15,11 +15,14 @@
 
 """
 
-import rospy 
+import cv_bridge
+import rospy
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import PointCloud2
 import cv2
 from cv_bridge import CvBridge
 import math
+import numpy as np
 
 class img_detect:
 
@@ -27,13 +30,19 @@ class img_detect:
 
         # Class members for depth camera image and flag for determining if message is sent
         self.img = None
-        self.ready = False
+        self.ready_img = False
+        self.ready_depth = False
+        self.depth_img = None 
+        self.bridge = CvBridge()
 
         # Initialize computer vision node
         rospy.init_node("CV_Node", anonymous = False, log_level = rospy.INFO)
 
         # Subscribe to the image topic
         self.image_sub = rospy.Subscriber("/bowser2/bowser2_dc/image/image", Image, callback=self.img_callback)
+
+        # Subscribe to depth image topic 
+        self.depth_sub = rospy.Subscriber("/bowser2/bowser2_dc/depth/image", Image, callback = self.depth_callback)
 
         # todo: figure out what message type we're publishing
         # self.location_pub = 
@@ -46,7 +55,14 @@ class img_detect:
 
     def img_callback(self, img_msg):
         self.img = img_msg
-        self.ready = True
+        self.ready_img = True
+
+    def depth_callback(self, depth_msg):
+        # self.depth_img = depth_msg
+        self.depth_img = self.bridge.imgmsg_to_cv2(depth_msg)
+        # print(self.depth_img)
+        self.ready_depth = True
+
 
     # Helper method to calculate circularity     
     def get_circularity(self, perimeter, area):
@@ -69,7 +85,7 @@ class img_detect:
         while not rospy.is_shutdown():
 
             # If the image hasn't been published, we don't do anything
-            if not self.ready:
+            if (not self.ready_img) and (not self.ready_depth):
                 continue
 
             # Convert image from ROS image to cv2 image
@@ -82,7 +98,7 @@ class img_detect:
             cv_img = cv2.GaussianBlur(cv_img, (3,3), 0)
 
             # Cut the image in half. 
-            cv_img = cv_img[int(len(cv_img)/2) + 20:]
+            #cv_img = cv_img[int(len(cv_img)/2) + 20:]
 
             # Apply simple thresholding to the image, to remove everything that isn't close to white. 
             ret, cv_img = cv2.threshold(cv_img, 125, 255, 0) 
@@ -95,26 +111,39 @@ class img_detect:
 
             for i in contours:
                 
-                # Get the area and peremiter, then use it to calculate circularity.
+                # Get the area and perimeter, then use it to calculate circularity.
                 area = cv2.contourArea(i)
                 perimeter = cv2.arcLength(i, True)
                 circularity = self.get_circularity(perimeter, area)
                 
                 #  Add to contour array if the contour is circular, and the contour is the size that we want.
-                if 100 < area < 6000 and circularity > 0.3:
+                if 100 < area < 20000 and circularity > 0.4:
                     validContours.append(i)
             
+            if not len(validContours) <= 0:
+                for contour in validContours:
+                    print("------------------------------")
+                    for point in contour:
+                        point = np.squeeze(point)
+                        try:
+                            print(self.depth_img[point[1]][point[0]])
+                        except IndexError:
+                            print("Index Err: {} {}".format(point[1], point[0]))
+                        except TypeError:
+                            print("fuck you")
+
             # Convert the image to color, so we can draw on it properly
-            cv_img = cv2.cvtColor(cv_img, cv2.COLOR_GRAY2BGR) 
+            cv_img = cv2.cvtColor(cv_img, cv2.COLOR_GRAY2BGR)
             
-            # Highlight the detected contours 
+            # Draw the contours 
             cv2.drawContours(cv_img, validContours, -1, (0,250,0), 3)
+
+            # Find the location in the depth image
             
             # Show what's going on as a sanity check
             self.show_image(cv_img)
-            cv2.waitKey(1)
-
-            self.ready = False
+            self.ready_img = False
+            self.ready_depth = False
 
         
 
