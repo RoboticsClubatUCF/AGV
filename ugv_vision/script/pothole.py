2f73e2bedee2e968db305182a3212feaf092e9df
Marc <mas1660@knights.ucf.edu>
2022-03-29 18:49:36 -0400

    Attempts to publish stamped polygon message

--- ugv_vision/script/pothole.py
+++ ugv_vision/script/pothole.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# Marc Simmonds 
-# IGVC 2021  
+# Marc Simmonds
+# IGVC 2022
 """ 
 
     This script is meant to:
@@ -15,6 +15,7 @@
 
 import rospy
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Polygon, PolygonStamped, Point32
 import cv2
 from cv_bridge import CvBridge
 import math
@@ -32,23 +33,19 @@ class pothole_detect:
         self.ready_depth = False
         self.depth_img = None
         self.bridge = CvBridge()
+        self.frame = "bowser2/camera_link"
 
-        # Initialize the node we'll be publishing from.
-        rospy.init_node("Potholes", anonymous = False, log_level = rospy.INFO)
+        rospy.init_node("Detected Obstacles", anonymous = False, log_level = rospy.INFO)
 
-        # Subscribe to the image topic
         self.image_sub = rospy.Subscriber("/bowser2/bowser2_dc/image/image", Image, callback=self.img_callback)
-
-        # Subscribe to depth image topic 
+ 
         self.depth_sub = rospy.Subscriber("/bowser2/bowser2_dc/depth/image", Image, callback = self.depth_callback)
 
-        # todo: figure out what message type we're publishing
+        self.pub = rospy.Publisher("Potholes", Polygon, queue_size = 3)
     
-    # Simple function for showing current image 
     def show_image(self, img):
         cv2.imshow("Image Window", img)
         cv2.waitKey(1)
-
 
     def img_callback(self, img_msg):
         self.img = self.bridge.imgmsg_to_cv2(img_msg)
@@ -58,19 +55,19 @@ class pothole_detect:
         self.depth_img = self.bridge.imgmsg_to_cv2(depth_msg)
         self.ready_depth = True
 
-
-    # Helper method to calculate circularity     
-    def get_circularity(self, perimeter, area):
+    def isPothole(self, perimeter, area):
         
         if perimeter == 0 or area == 0:
-            return 0
+            return False
 
-        return (4*math.pi*area)/(math.pow(perimeter,2))
+        circularity = (4*math.pi*area)/(math.pow(perimeter,2))
+
+        if circularity > 0.5 and (75 < area < 20000):
+            return True
 
     # The angular distance is correct and the trig checks out, but this still doesn't work correctly for some reason. 
     def getLocation(self, coordinate, depth):
         
-        # If we have some garbage values, we simply return an error
         if depth > 19:
             return self.ERR
         
@@ -78,6 +75,7 @@ class pothole_detect:
         # multiplied by the horizontal distance of the pixel from the center of the frame.
         theta = ((45/640) * abs(coordinate[0] - 320))
 
+        # Point is in center of frame
         if theta == 0:
             return 0
 
@@ -98,20 +96,16 @@ class pothole_detect:
     # Runs the circle detection process
     def getPotholes(self):
         
-        # Runs while the ros is up
         while not rospy.is_shutdown():
 
             # If the image hasn't been published, we don't do anything
             if (not self.ready_img) and (not self.ready_depth):
                 continue
 
-            # Convert image from ROS Image type to cv2 image
             cv_img = self.img
 
-            # Apply simple thresholding to the image, to remove everything that isn't close to white 
             ret, cv_img = cv2.threshold(cv_img, 125, 255, 0)
-            
-            # Convert the image to grey so we can use the findContours function
+
             cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
             
             # Find the contours in the image
@@ -122,43 +116,44 @@ class pothole_detect:
             validContours = []
             for i in contours:
                 
-                # Get the area and perimeter, then use it to calculate circularity
                 area = cv2.contourArea(i)
                 perimeter = cv2.arcLength(i, True)
-                circularity = self.get_circularity(perimeter, area)
                 
-                #  Add to contour array if the contour is circular, and the contour is the size that we want
-                if ((100 < area < 20000) and (circularity > 0.4)) or 50 < area < 500:
+                if self.isPothole(perimeter, area):
                     validContours.append(i)
              
-            # Get the locations of the points in the circle by getting the pixels in the depth image, if our contour array isn't empty
             if not len(validContours) <= 0:
+                # Polygon
                 for contour in validContours:
-                    print("------------------------------")
+                    p = PolygonStamped()
+                    p.header = self.frame
+                    # Point
                     for point in contour:
                         point = np.squeeze(point)
                         try:
                             # Try to get the point in the contour in the depth image
                             # Right now, this just prints the location. Sometimes it prints the max range of the sensor, 
                             # which means it isn't detecting anything. Other times, weird shit happens.
-                            
+                            xPoint = self.getLocation(point, self.depth_img[point[1]][point[0]])
+                            zPoint = self.depth_img[point[1]][point[0]]
+                            p.polygon.points = Point32(x=xPoint, y=0.000, z = zPoint)
                             # Prints (z, x). y is assumed to be the ground plane.
-                            print(self.depth_img[point[1]][point[0]], self.getLocation(point, self.depth_img[point[1]][point[0]]))
+                            #print(self.depth_img[point[1]][point[0]], self.getLocation(point, self.depth_img[point[1]][point[0]]))
                         
                         except IndexError:
                             continue
                         
                         except TypeError:
                             continue
+                    self.pub.publish(p)
 
-
-            # Convert the image to color, so we can draw on it properly
-            cv_img = cv2.cvtColor(cv_img, cv2.COLOR_GRAY2BGR)
-            
-            # Draw the contours
-            cv2.drawContours(cv_img, validContours, -1, (0,250,0), 3)
 
             # Show what's going on as a sanity check
+
+            cv_img = cv2.cvtColor(cv_img, cv2.COLOR_GRAY2BGR)
+            
+            cv2.drawContours(cv_img, validContours, -1, (0,250,0), 3)
+
             self.show_image(cv_img)
             self.ready_img = False
             self.ready_depth = False
