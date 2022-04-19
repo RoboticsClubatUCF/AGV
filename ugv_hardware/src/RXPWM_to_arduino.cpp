2af6ee23f859d866abe2b1a60ea85778e1a6e9ff
Tevin Mukudi <64235935+Ven011@users.noreply.github.com>
2022-04-18 20:02:45 -0400

    recent

--- ugv_hardware/src/RXPWM_to_arduino.cpp
+++ ugv_hardware/src/RXPWM_to_arduino.cpp
@@ -1,36 +1,54 @@
 #include <Arduino.h>
 
-#define rx1 2
-#define rx2 3
-#define rx3 4
-#define rx4 5
-#define rx5 6
-#define rx6 7
+#define RJ_x 2 // right joystick x-axis - pin 6 on receiver
+#define RJ_y 3
+#define LJ_y 4
+#define LJ_x 5
+#define ESTOP 6
+#define DIAL 7
 
-void setup() {
+// function definitions
+String get_input(int pin);
+
+void setup(){
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(rx1, INPUT);
-  pinMode(rx2, INPUT);
-  pinMode(rx3, INPUT);
-  pinMode(rx4, INPUT);
-  pinMode(rx5, INPUT);
-  pinMode(rx6, INPUT);
+  pinMode(RJ_x, INPUT);
+  pinMode(RJ_y, INPUT);
+  pinMode(LJ_y, INPUT);
+  pinMode(LJ_x, INPUT);
+  pinMode(ESTOP, INPUT);
+  pinMode(DIAL, INPUT);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
 
-  Serial.print(pulseIn(rx1, HIGH));
-  Serial.print("    ");
-  Serial.print(pulseIn(rx2, HIGH));
-  Serial.print("    ");
-  Serial.print(pulseIn(rx3, HIGH));
-  Serial.print("    ");
-  Serial.print(pulseIn(rx4, HIGH));
-  Serial.print("    ");
-  Serial.print(pulseIn(rx5, HIGH));
-  Serial.print("    ");
-  Serial.println(pulseIn(rx6, HIGH));
+  Serial.print("A");
+  Serial.print(get_input(RJ_x));
+  Serial.print("B");
+  Serial.print(get_input(RJ_y));
+  Serial.print("C");
+  Serial.print(get_input(LJ_y));
+  Serial.print("D");
+  Serial.print(get_input(LJ_x));
+  Serial.print("E");
+  Serial.print(get_input(ESTOP));
+  Serial.print("F");
+  Serial.print(get_input(DIAL));
+}
+
+String get_input(int pin){
+  // use pulseIn to turn PWM signal to values within a range
+  int input = pulseIn(pin, HIGH);
+  
+  // turn the input to a string
+  String s_input = String(input);
+
+  // make sure input is 4 bytes
+  if(s_input.length() < 4){
+    s_input += "_";
+  }
+
+  return s_input;
 }
