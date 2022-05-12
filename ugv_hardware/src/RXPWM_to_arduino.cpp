eb2ab209d55cb97c71eb47283d2617fbf36b1004
Wesley Fletcher <wesfletch@gmail.com>
2022-05-12 19:27:28 -0400

    cleanup and docs

--- ugv_hardware/src/RXPWM_to_arduino.cpp
+++ ugv_hardware/src/RXPWM_to_arduino.cpp
@@ -1,45 +1,90 @@
+/**
+ * @file RXPWM_to_arduino.cpp
+ * @author Wesley Fletcker, Tevin Mukudi
+ * @brief 
+ * @version 0.1
+ * @date 2022-05-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <Arduino.h>
 
-#define RJ_x 2 // right joystick x-axis - pin 6 on receiver
-#define RJ_y 3
-#define LJ_y 4
-#define LJ_x 5
-#define ESTOP 6
-#define DIAL 7
+#define RC_RJ_x 2 // right joystick x-axis - pin 6 on receiver
+#define RC_RJ_y 3
+#define RC_LJ_y 4
+#define RC_LJ_x 5
+#define RC_ESTOP 6
+#define RC_DIAL 7
 
-// function definitions
+// function prototypes
 String get_input(int pin);
+void print_rc_inputs();
 
-void setup(){
-  // put your setup code here, to run once:
+int rc_in[6] = {0,0,0,0,0,0}; // hold RC inputs
+int hz = 100;  // loop rate
+
+/**
+ * @brief Run once.
+ * 
+ */
+void setup()
+{  
+  // set-up serial connection
   Serial.begin(9600);
-  pinMode(RJ_x, INPUT);
-  pinMode(RJ_y, INPUT);
-  pinMode(LJ_y, INPUT);
-  pinMode(LJ_x, INPUT);
-  pinMode(ESTOP, INPUT);
-  pinMode(DIAL, INPUT);
 
+  // configure RC pins as inputs
+  pinMode(RC_RJ_x, INPUT);
+  pinMode(RC_RJ_y, INPUT);
+  pinMode(RC_LJ_y, INPUT);
+  pinMode(RC_LJ_x, INPUT);
+  pinMode(RC_ESTOP, INPUT);
+  pinMode(RC_DIAL, INPUT);
 }
 
-void loop(){
+/**
+ * @brief Run continuously.
+ * 
+ */
+void loop()
+{
+  // print RC inputs to the Jetson
+  print_rc_inputs();
 
-  Serial.print("A");
-  Serial.print(get_input(RJ_x));
-  Serial.print("B");
-  Serial.print(get_input(RJ_y));
-  Serial.print("C");
-  Serial.print(get_input(LJ_y));
-  Serial.print("D");
-  Serial.print(get_input(LJ_x));
-  Serial.print("E");
-  Serial.print(get_input(ESTOP));
-  Serial.print("F");
-  Serial.print(get_input(DIAL));
-  Serial.print("G");
+  // attempt to maintain loop rate
+  delay(1000 / hz); 
 }
 
-String get_input(int pin){
+/**
+ * @brief Print the current value of RC pins to Jetson
+ * 
+ */
+void print_rc_inputs()
+{
+  Serial.print("$RCX");
+  Serial.print(" ");
+  Serial.print(get_input(RC_RJ_x));
+  Serial.print(" ");
+  Serial.print(get_input(RC_RJ_y));
+  Serial.print(" ");
+  Serial.print(get_input(RC_LJ_y));
+  Serial.print(" ");
+  Serial.print(get_input(RC_LJ_x));
+  Serial.print(" ");
+  Serial.print(get_input(RC_ESTOP));
+  Serial.print(" ");
+  Serial.print(get_input(RC_DIAL));
+  Serial.print("\n");
+}
+
+/**
+ * @brief Read PPM data from a single pin using pulseIn()
+ * 
+ * @param pin pin to be read
+ * @return String
+ */
+String get_input(int pin)
+{
   // use pulseIn to turn PWM signal to values within a range
   int input = pulseIn(pin, HIGH);
   
